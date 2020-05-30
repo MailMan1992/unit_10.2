b35594134c7db727b4145c4925ad7d92bd6e8da2
MailMan1992 <64146310+MailMan1992@users.noreply.github.com>
2020-05-30 20:52:14 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+
+@app.route('/')
+@app.route('/homepage')
+def home_page():
+    return render_template("homepage.html")
+
+
+@app.route('/gallery')
+# def gallery():
+#     images = os.listdir(os.path.join(app.static_folder, 'gallery_images'))
+#     return render_template("gallery.html", images=images)
+def portfolio():
+    images = os.listdir(os.path.join(app.static_folder, "gallery_images"))
+    return render_template('gallery.html', images=images)
+
+
+
+@app.route('/reviews')
+def review_page():
+    return render_template("review-page.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/nav.js
@@ -0,0 +1,45 @@
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+
+}
+
+navSlide();
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
